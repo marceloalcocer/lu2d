8841a40f694dbee2e4f8da73ebcacf5c9b83b776
Marcelo Alcocer <marcelo.j.p.alcocer@gmail.com>
2021-11-22 14:13:19 +0100

    Add examples

---
+++ examples/binary_io.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import lu2d
+import numpy as np
+
+
+def dataset_from_binary():
+	"""Example of reading a dataset from binary files"""
+
+	# Read dataset
+	path = "data/binary/demo.ini"
+	dataset = lu2d.Dataset.from_binary(path)
+
+	# Signal
+	signal = dataset.signal
+
+	# Axes
+	t1, t2, w3 = dataset.axes
+	assert len(dataset.axes) == dataset.signal.ndim
+	assert len(t1) == signal.shape[0]
+	assert len(t2) == signal.shape[1]
+	assert len(w3) == signal.shape[2]
+
+	# Local ocillators
+	los = dataset.los
+	assert len(los) == len(t2)
+	for lo in los:
+		assert len(lo) == len(w3)
+
+	# Timestamps
+	timestamps = dataset.timestamps
+	assert len(timestamps) == len(t2)
+
+	# Excitation energy
+	excitation_energy = dataset.excitation_energy
+
+	# Repetition rate
+	repetition_rate = dataset.repetition_rate
+
+	# Polarizations
+	polarizations = dataset.polarizations
+	assert len(polarizations) == 4
+
+	# FS thickness
+	fs_thickness = dataset.fs_thickness
+
+	# All metadata
+	scans_per_point = dataset.metadata["Header"]["ScansPerPoint"]
+	experiment_2_path = dataset.metadata["Experiment 2"]["File2DSignal"]
+	
+	return dataset
+
+if __name__ == "__main__":
+	dataset_from_binary()
---
+++ examples/text_io.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import lu2d
+import numpy as np
+
+
+def dataset_from_text():
+	"""Example of reading a dataset from plain-text files"""
+
+	# Read dataset
+	fmtstrs = {
+		"real": "data/text/demo_Real_{t2}fs.txt",
+		"imag": "data/text/demo_Imaginary_{t2}fs.txt"
+	}
+	t2 = [0, 100, 1000, 900000]
+	dataset = lu2d.Dataset.from_text(fmtstrs, t2)
+
+	# Signal
+	signal = dataset.signal
+
+	# Axes
+	t1, t2, w3 = dataset.axes
+	assert len(dataset.axes) == dataset.signal.ndim
+	assert len(t1) == signal.shape[0]
+	assert len(t2) == signal.shape[1]
+	assert len(w3) == signal.shape[2]
+	
+	return dataset
+
+def dataset_to_text():
+	"""Example of writing a dataset to plain-text files"""
+
+	# Example dataset
+	dataset = dataset_from_text()
+
+	# File specification
+	fmtstrs = {
+		"real": "data/text/write_Real_{t2}fs.txt",
+		"imag": "data/text/write_Imaginary_{t2}fs.txt"
+	}
+
+	# Write dataset
+	dataset.to_text(fmtstrs)
+
+
+if __name__ == "__main__":
+	dataset_from_text()
+	dataset_to_text()
